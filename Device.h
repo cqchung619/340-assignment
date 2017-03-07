f18601f61a13826615d0086afed9a2bfd59fbb47
Cuong Chung <cuong.chung21@myhunter.cuny.edu>
2017-03-07 07:07:05 -0500

    PCB added class members and getters/setters.

--- Device.h
+++ Device.h
@@ -14,17 +14,16 @@ public:
     const std::string &Get_Name() { return name_; }
 
     // Add process to the back of the queue.
-    void Add_Process(const PCB &a_process) { queue_.push(a_process); }
+    void Add_Process(const PCB &a_process);
 
     // Remove process in the front of the queue.
-    void Remove_Current_Process() { queue_.pop(); }
+    void Remove_Current_Process();
 
     // Returns a const reference to the process in front of the queue.
-    const PCB &Current_Process() { return queue_.front(); }
+    const PCB &Current_Process();
 
 private:
     std::string name_;
-    std::queue<PCB> queue_;
 };
 
 #endif
--- PCB.h
+++ PCB.h
@@ -1,8 +1,40 @@
+/*
+Author: Cuong Chung
+Course: CSCI 34000
+Instructor: Eric Schweitzer
+Assignment #1: Basic data structures of OS
+*/
+
 #ifndef PCB_H
 #define PCB_H
 
-class PCB {
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
+class PCB {
+public:
+    enum State { NEW, READY, RUNNING, WAITING };
+
+    PCB(const unsigned int a_PID, const unsigned int a_mem_location): state_{PCB::NEW}, PID_(a_PID), memstart_{a_mem_location} {}
+
+    string &Get_State();
+    void Change_State(const State new_state) { state_ = new_state; }
+
+    const unsigned int &Get_PID() const { return PID_; }
+
+    unsigned int &Get_Memstart() const { return memstart_; }
+    void Change_Memstart(const unsigned int new_mem_location) { memstart_ = new_mem_location; }
+
+    string &Get_Parameters();
+    
+
+private:
+    State state_;
+    const unsigned int PID_;
+    unsigned int memstart_;
+    vector<string> parameters_;
 };
 
 #endif
