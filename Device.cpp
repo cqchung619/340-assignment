8c75d97a6af6914c79be4e669c51823096629e25
Cuong Chung <cuong.chung21@myhunter.cuny.edu>
2017-03-12 11:49:22 -0400

    Change PCBQueue member variable to a pointer.

--- Device.cpp
+++ Device.cpp
@@ -2,7 +2,7 @@
 
 ostream &operator<<(ostream &out, const Device &a_device) {
     out << "-----" << a_device.name_ << "-----" << endl;
-    a_device.process_queue_.Output_Processes(out, PCB::DEVICE);
+    a_device.process_queue_->Output_Processes(out, PCB::DEVICE);
 
     return out;
 }
--- Device.h
+++ Device.h
@@ -17,31 +17,31 @@ using namespace std;
 
 class Device {
 public:
-    Device(const string &a_name): name_{a_name} {}
+    Device(const string &a_name): name_{a_name}, process_queue_{new PCBQueue} {}
 
     // Returns true if process_queue_ is empty (Device has no requests), otherwise returns false.
-    bool Is_Idle() { return process_queue_.empty(); }
+    bool Is_Idle() { return process_queue_->empty(); }
 
     // Returns const reference to name_;
     const string &Get_Name() const { return name_; }
 
     // Add process to the back of the queue.
-    void Add_Process(PCB *a_process) { process_queue_.enqueue(a_process); }
+    void Add_Process(PCB *a_process) { process_queue_->enqueue(a_process); }
 
     // Remove and returns the process in the front of the queue.
     // Assumes queue is non-empty.
-    PCB *Remove_Running_Process() { return process_queue_.dequeue(); }
+    PCB *Remove_Running_Process() { return process_queue_->dequeue(); }
 
     // Returns a const reference to the process in front of the queue.
     // Assumes queue is non-empty.
-    const PCB *Running_Process() { return process_queue_.front(); }
+    const PCB *Running_Process() { return process_queue_->front(); }
 
     // Operator<< overload.
     friend ostream &operator<<(ostream &out, const Device &a_device);
 
 protected:
     string name_;
-    PCBQueue process_queue_;
+    PCBQueue *process_queue_;
 };
 
 #endif
