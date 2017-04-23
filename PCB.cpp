5b6f6c259b14365009575e5ba963dbdc6923a351
Cuong Chung <cuong.chung21@myhunter.cuny.edu>
2017-04-23 11:03:17 -0400

    Added accounting stats.

--- PCB.cpp
+++ PCB.cpp
@@ -1,12 +1,38 @@
 #include "PCB.h"
 
-void PCB::Output_PCB(ostream &out, OutputFormat format) const{
+void PCB::Update_Process_Stats(double execution_time) {
+    stats_.cpu_usage_time += execution_time;
+    stats_.actual_burst_time += execution_time;
+    stats_.burst_time_left -= execution_time;
+}
+
+void PCB::Update_Process_Stats(double execution_time, double history_parameter) {
+    stats_.cpu_usage_time += execution_time;
+    stats_.actual_burst_time += execution_time;
+    stats_.estimated_burst_time = (history_parameter * stats_.estimated_burst_time) +
+                                 ( (1 - history_parameter)  * stats_.actual_burst_time);
+    stats_.burst_time_left = stats_.estimated_burst_time;
+    stats_.total_burst_time += stats_.actual_burst_time;
+    ++stats_.burst_count;
+    stats_.actual_burst_time = 0;
+}
+
+double PCB::Get_Average_Burst_Time() {
+    if (stats_.burst_count == 0) {
+        return 0;
+    }
+
+    return stats_.total_burst_time / (double) stats_.burst_count;
+}
+
+void PCB::Output_PCB(ostream &out, OutputFormat format){
     switch (format) {
         case CPU:
             out << right
                 << setw(5) << PID_
-                << setw(15) << cpu_usage_time_
-                << setw(15) << average_burst_time_;
+                << setw(15) << Get_CPU_Usage_Time()
+                << left
+                << setw(15) << ( " " + to_string(Get_Average_Burst_Time()) );
             break;
         case DEVICE:
             out << right
@@ -18,8 +44,24 @@ void PCB::Output_PCB(ostream &out, OutputFormat format) const{
                 << setw(5) << parameters_[2]
                 << left
                 << setw(20) << ( " " + parameters_[3] )
-                << setw(15) << ( " " + to_string(cpu_usage_time_) )
-                << setw(15) << ( " " + to_string(average_burst_time_) );
+                << setw(15) << ( " " + to_string(Get_CPU_Usage_Time()) )
+                << left
+                << setw(15) << ( " " + to_string(Get_Average_Burst_Time()) );
+            break;
+        case DISK:
+            out << right
+                << setw(5) << PID_
+                << left
+                << setw(20) << ( " " + parameters_[0] )
+                << right
+                << setw(10) << parameters_[1]
+                << setw(5) << parameters_[2]
+                << left
+                << setw(20) << ( " " + parameters_[3] )
+                << setw(20) << ( " " + parameters_[4] )
+                << setw(15) << ( " " + to_string(Get_CPU_Usage_Time()) )
+                << left
+                << setw(15) << ( " " + to_string(Get_Average_Burst_Time()) );
             break;
     }
 
--- PCB.h
+++ PCB.h
@@ -16,11 +16,18 @@ using namespace std;
 
 class PCB {
 public:
-    enum OutputFormat { CPU, DEVICE };
+    enum OutputFormat { CPU, DEVICE, DISK };
 
     // @a_PID: OS assigned integer value.
     // All newly constructed PCBs are in the NEW state.
-    PCB(const unsigned int a_PID): PID_{a_PID}, cpu_usage_time_{0}, average_burst_time_{0} {}
+    PCB(const unsigned int a_PID, double initial_burst): PID_{a_PID} {
+        stats_.cpu_usage_time = 0;
+        stats_.actual_burst_time = 0;
+        stats_.estimated_burst_time = initial_burst;
+        stats_.burst_time_left = stats_.estimated_burst_time;
+        stats_.total_burst_time = 0;
+        stats_.burst_count = 0;
+    }
 
     // @rhs: PCB to be copied.
     // Constructs a copy of rhs.
@@ -29,30 +36,40 @@ public:
     // PID accessor. PID never changes.
     const unsigned int &Get_PID() const { return PID_; }
 
-    // cpu_usage_time_ get/set
-    unsigned int Get_CPU_Usage_Time() { return cpu_usage_time_; }
-    void Set_CPU_Usage_Time(const unsigned int new_time) { cpu_usage_time_ = new_time; }
-
-    // average_burst_time_ get/set
-    unsigned int Get_Average_Burst_Time() { return average_burst_time_; }
-    void Set_Average_Burst_Time(const unsigned int new_time) { average_burst_time_ = new_time; }
-
     // Parameter accessor and modifier.
     const vector<string> &Get_Parameters() { return parameters_; }
+    const string &Get_Parameter(const unsigned int index) { return parameters_[index]; }
     void Add_Param(const string &a_param) { parameters_.push_back(a_param); }
     void Clear_Params() { parameters_.clear(); }
 
+    double Get_CPU_Usage_Time() { return stats_.cpu_usage_time; }
+    double Get_Average_Burst_Time();
+    double Get_Burst_Time_Left() { return stats_.burst_time_left; }
+
+    // Interrupts
+    void Update_Process_Stats(double execution_time);
+    // System Calls
+    void Update_Process_Stats(double execution_time, double history_parameter);
+
     // Outputs the PCB to out in the specified format.
     // If format is DEVICE, it assumes parameters_ has at least 4 elements.
-    void Output_PCB(ostream &out, OutputFormat format) const;
+    void Output_PCB(ostream &out, OutputFormat format);
 
 private:
+    struct AccountingInformation {
+        double cpu_usage_time;
+        double actual_burst_time;
+        double estimated_burst_time;
+        double burst_time_left;
+        double total_burst_time;
+        unsigned int burst_count;
+
+    } stats_;
+
     const unsigned int PID_;
-    unsigned int cpu_usage_time_;
-    unsigned int average_burst_time_;
 
     // Contents should be:
-    // Filename , Memstart , R/W , File Length
+    // Filename , Memstart , R/W , File Length, Cylinder(Disk Only)
     // in this exact order.
     vector<string> parameters_;
 };
