7009ff7ed669d5ceb406fdb7eb92916aa9f0f243
Cuong Chung <cuong.chung21@myhunter.cuny.edu>
2017-04-23 18:25:39 -0400

    minor edits

--- Disk.h
+++ Disk.h
@@ -3,21 +3,6 @@ Author: Cuong Chung
 Course: CSCI 34000
 Instructor: Eric Schweitzer
 Assignment #2: Pre-Emptive SJF, Disk Scheduling and Accounting
-
-A representation of a disk device. This class uses a circular linked list to implement a disk queue.
-
-The circular linked list simulates the CLOOK disk scheduling algorithm. Each process is scheduled in
-ascending order based on the cylinder accessed. The seek head is always traveling inwards
-(i.e. higher cylinder number). When there is no job towards the inner cylinders, the seek head circles
-back to the first job on the outer edge. In the circular linked list, the last job points to the first
-job of the outer edge, so seek head progression is simple and just keeps moving foward.
-
-Jobs accessing the same cylinder is scheduled as FCFS(First Come First Serve). The new job goes after
-the last job accessing the same cylinder. The exception is if the current seek head is at that cynlinder
-and the next job is not accessing the saem cylinder. In this case, the job goes behind the seek head. In
-the circular queue, this means that the job is inserted before the seek head position.
-
-
 */
 
 #ifndef DISK_H
@@ -55,7 +40,7 @@ public:
 
 private:
     const unsigned int CYLINDER_COUNT;
-    DiskQueue *process_queue_;    
+    DiskQueue *process_queue_;
 };
 
 #endif
