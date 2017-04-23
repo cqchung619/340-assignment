abdbcd4c2e6f0f5de9bc3f826ccc7fad08976e60
Cuong Chung <cuong.chung21@myhunter.cuny.edu>
2017-04-23 11:08:19 -0400

    Converted to pure virtual class.

--- Device.cpp
+++ Device.cpp
@@ -1,8 +1,1 @@
 #include "Device.h"
-
-ostream &operator<<(ostream &out, const Device &a_device) {
-    out << "-----" << a_device.name_ << "-----" << endl;
-    a_device.process_queue_->Output_Processes(out, PCB::DEVICE);
-
-    return out;
-}
--- Device.h
+++ Device.h
@@ -2,7 +2,7 @@
 Author: Cuong Chung
 Course: CSCI 34000
 Instructor: Eric Schweitzer
-Assignment #2: Pre-Emptive SJF, Disk Scheduling and Accounting
+Assignment #2: Pre-Emptive SJF, Disk Scheduling and Accounting.
 */
 
 #ifndef DEVICE_H
@@ -17,32 +17,31 @@ using namespace std;
 
 class Device {
 public:
-    Device(const string &a_name): name_{a_name}, process_queue_{new PCBQueue} {}
+    Device(const string &a_name): name_{a_name} {}
     virtual ~Device() {}
-
-    // Returns true if process_queue_ is empty (Device has no requests), otherwise returns false.
-    bool Is_Idle() { return process_queue_->empty(); }
 
     // Returns const reference to name_;
     const string &Get_Name() const { return name_; }
 
+    // Returns true if process_queue_ is empty (Device has no requests), otherwise returns false.
+    virtual bool Is_Idle() = 0;
+
     // Add process to the back of the queue.
-    virtual void Add_Process(PCB *a_process) { process_queue_->enqueue(a_process); }
+    virtual void Add_Process(PCB *a_process) = 0;
 
     // Remove and returns the process in the front of the queue.
     // Assumes queue is non-empty.
-    PCB *Remove_Running_Process() { return process_queue_->dequeue(); }
+    virtual PCB *Remove_Running_Process() = 0;
 
     // Returns a const reference to the process in front of the queue.
     // Assumes queue is non-empty.
-    const PCB *Running_Process() { return process_queue_->front(); }
+    virtual const PCB *Running_Process() = 0;
 
     // Operator<< overload.
     friend ostream &operator<<(ostream &out, const Device &a_device);
 
 protected:
     string name_;
-    PCBQueue *process_queue_;
 };
 
 #endif
