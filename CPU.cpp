44de3edb832901356910d3000c6d38320d54e9b0
Cuong Chung <cuong.chung21@myhunter.cuny.edu>
2017-04-23 18:19:56 -0400

    minor edits

--- CPU.cpp
+++ CPU.cpp
@@ -6,12 +6,3 @@ PCB* CPU::Unbind_Process() {
 
     return a_process;
 }
-
-// ostream &operator<<(ostream &out, const CPU &a_CPU) {
-//     out << "-----CPU Processes-----" << endl;
-//     out << left
-//         << setw(5) << "PID" << endl;
-//     a_CPU.process_queue_.Output_Processes(out, PCB::CPU);
-//
-//     return out;
-// }
