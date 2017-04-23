48194856e370781b27295188d41337a322ba8b50
Cuong Chung <cuong.chung21@myhunter.cuny.edu>
2017-04-23 18:20:18 -0400

    Updated output format.

--- PCB.cpp
+++ PCB.cpp
@@ -30,38 +30,37 @@ void PCB::Output_PCB(ostream &out, OutputFormat format){
         case CPU:
             out << right
                 << setw(5) << PID_
-                << setw(15) << Get_CPU_Usage_Time()
-                << left
-                << setw(15) << ( " " + to_string(Get_Average_Burst_Time()) );
+                << setw(20) << (double) Get_CPU_Usage_Time()
+                << setw(20) << (double) Get_Average_Burst_Time();
             break;
         case DEVICE:
             out << right
-                << setw(5) << PID_
+                << setw(3) << PID_
                 << left
-                << setw(20) << ( " " + parameters_[0] )
+                << setw(9) << ( "  " + parameters_[0] )
                 << right
                 << setw(10) << parameters_[1]
                 << setw(5) << parameters_[2]
                 << left
-                << setw(20) << ( " " + parameters_[3] )
-                << setw(15) << ( " " + to_string(Get_CPU_Usage_Time()) )
-                << left
-                << setw(15) << ( " " + to_string(Get_Average_Burst_Time()) );
+                << setw(13) << ( "  " + parameters_[3] )
+                << right
+                << setw(14) << (double) Get_CPU_Usage_Time()
+                << setw(16) << (double) Get_Average_Burst_Time();
             break;
         case DISK:
             out << right
-                << setw(5) << PID_
+                << setw(3) << PID_
                 << left
-                << setw(20) << ( " " + parameters_[0] )
+                << setw(9) << ( "  " + parameters_[0] )
                 << right
                 << setw(10) << parameters_[1]
                 << setw(5) << parameters_[2]
                 << left
-                << setw(20) << ( " " + parameters_[3] )
-                << setw(20) << ( " " + parameters_[4] )
-                << setw(15) << ( " " + to_string(Get_CPU_Usage_Time()) )
-                << left
-                << setw(15) << ( " " + to_string(Get_Average_Burst_Time()) );
+                << setw(13) << ( "  " + parameters_[3] )
+                << setw(10) << ( "  " + parameters_[4] )
+                << right
+                << setw(14) << (double) Get_CPU_Usage_Time()
+                << setw(16) << (double) Get_Average_Burst_Time();
             break;
     }
 
