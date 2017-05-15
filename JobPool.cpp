31223a60ada01d61091b083a6d76158e862e0282
Cuong Chung <cuong.chung21@myhunter.cuny.edu>
2017-05-15 10:22:06 -0400

    Implmeneted Find_Largest_Process() and Remove_Process().

--- JobPool.cpp
+++ JobPool.cpp
@@ -1,6 +1,20 @@
 #include "JobPool.h"
 
 PCB *JobPool::Find_Largest_Process(const unsigned int size_limit) {
+    unsigned int largest_page_table_size = 0;
+    for (auto item : pool_) {
+        if (item.first <= size_limit) {
+            largest_page_table_size = item.first;
+        } else {
+            break;
+        }
+    }
+
+    if (largest_page_table_size == 0) {
+        return nullptr;
+    } else {
+        return pool_[largest_page_table_size].front();
+    }
 
 }
 
@@ -10,7 +24,12 @@ void JobPool::Add_Process(PCB *a_process) {
 }
 
 void JobPool::Remove_Pocess(PCB *a_process) {
-
+    unsigned int page_table_size = a_process->Get_Page_Table_Size();
+    if (pool_[page_table_size].size() == 1) {
+        pool_.erase(page_table_size);
+    } else {
+        pool_[page_table_size].pop_front();
+    }
 }
 
 void JobPool::Output_Job_Pool(ostream& out) {
